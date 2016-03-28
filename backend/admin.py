7d03d8f81bf2fb11c3eb4c58a6f4a8e2f217c577
Alexandrine Boissière <theasta@gmail.com>
2016-03-27 18:09:36 -0700

    Trying PointField

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.gis.admin import OSMGeoAdmin
+from django.contrib.gis.admin import OSMGeoAdmin, GeoModelAdmin
 
 from backend.models import Sepulture, Personality, Identity, Info, Country
 
